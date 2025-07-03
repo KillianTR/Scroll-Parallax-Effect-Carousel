e40316c17577f48a06bda3c5682bd6fd608ae7ba
Killian Torrell <161701378+KillianTR@users.noreply.github.com>
2025-07-03 10:26:13 +0200

    Elements essencials

---
+++ slides.js
@@ -0,0 +1,29 @@
+const slides = [
+{
+    tag: "Website",
+    marquee: "Eclipse Interactive Art Portfolio",
+    image: "src/slide-img-1.png",
+},
+{
+    tag: "Branding",
+    marquee: "Solaris Avant-Garde Brand Identity",
+    image: "src/slide-img-2.png",
+},
+{
+    tag: "Experience",
+    marquee: "Nova Immersive Light Exhibition",
+    image: "src/slide-img-3.png",
+},
+{
+    tag: "Website",
+    marquee: "Luminex Virtual Reality Showcase",
+    image: "src/slide-img-4.png",
+},
+{
+    tag: "Marketing",
+    marquee: "Orion Digital Art Launch Campaign",
+    image: "src/slide-img-5.png",
+}
+];
+
+export default slides;
